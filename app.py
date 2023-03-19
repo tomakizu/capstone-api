90e19b9a87b01cf867a5a336e24162add8297bc2
Thomas Chong <thomaschong0929@gmail.com>
2023-03-19 21:59:49 +0800

    updated predict def to post method

--- app.py
+++ app.py
@@ -1,12 +1,12 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 import os
 import prediction
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['POST'])
 def predict():
-    return prediction.getPredictionResult()
+    return prediction.getPredictionResult(dataset=request.json['dataset'], timestamp=request.json['timestamp'])
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8014, debug=True)
